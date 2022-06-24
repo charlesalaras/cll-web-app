eef80b3bf61f6404e8ac4dc5f43ff7fc284cbe96
Charles Alaras <charles.alaras@gmail.com>
2022-06-24 11:07:26 -0700

    Add inline AppBar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
 import Head from 'next/head'
-import Button from '@mui/material/Button'
-import clientPromise from '../lib/mongodb'
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import Box from '@mui/material/Box';
+import Tooltip from '@mui/material/Tooltip';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Button from '@mui/material/Button';
+import clientPromise from '../lib/mongodb';
+
+const pages = ["Dashboard", "Assignments", "Performance", "Practice"];
 
 export default function Home({ isConnected }) {
   return (
@@ -11,6 +25,52 @@ export default function Home({ isConnected }) {
       </Head>
 
       <main>
+	<AppBar position="static">
+	  <Container maxWidth="x1">
+	  <Toolbar disableGutters>
+	  <Typography
+	   variant="h6"
+	   noWrap
+	   component="a"
+	   href="/"
+	   sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+	   }}
+	  >
+	  CALIFORNIA LEARNING LABS
+	  </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+	  <Box sx={{ flexGrow: 0 }}>
+	    <FormControlLabel
+	    control={<Switch color="default"/>}
+	    label="Light/Dark Mode"
+	    />
+	  </Box>
+	  <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp"/>
+              </IconButton>
+	    </Tooltip>
+	  </Box>
+	  </Toolbar>
+	  </Container>
+	</AppBar>
         <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
         </h1>
