ac33e9c23d7ae39db288abf9f950e937c479d30a
Charles Alaras <charles.alaras@gmail.com>
2022-08-04 14:53:34 -0700

    Add basic authentication

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,37 @@
 import Head from 'next/head';
 import Button from '@mui/material/Button';
-import clientPromise from '../lib/mongodb';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import GoogleIcon from '@mui/icons-material/Google';
 import NavBar from '../components/NavBar';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
-export default function Home({ isConnected }) {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <NavBar/>
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
-        </h1>
-        {isConnected ? (
-          <h2 className="subtitle">You are connected to MongoDB</h2>
-        ) : (
-          <h2 className="subtitle">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
-            for instructions.
-          </h2>
-        )}
-        <Button variant="contained">Hello World!</Button>
-      </main>
-    </div>
-  )
+export default function Home() {
+    const { data: session } = useSession();
+    
+    return (
+        <div className="container">
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        {session ? 
+            <Card>
+                <CardContent>
+                    <Typography>You are logged in!</Typography>
+                </CardContent>
+                <Button onClick={() => signOut()}>Sign Out</Button>
+            </Card>
+            :
+            <Card>
+                <CardContent>
+                    <Button variant="outlined" startIcon={<GoogleIcon/>} onClick={() => signIn()}>Sign in with Google</Button>
+                    <Typography>* Please use Google account provided by educational institution</Typography>
+                </CardContent>
+            </Card>
+        }
+        </div>
+    )
 }
 
-export async function getServerSideProps(context) {
-  try {
-    await clientPromise
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
-
-    return {
-      props: { isConnected: true },
-    }
-  } catch (e) {
-    console.error(e)
-    return {
-      props: { isConnected: false },
-    }
-  }
-}
