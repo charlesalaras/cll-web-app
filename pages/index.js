82d2fd8ff4980d7f5ad83b7a32f7dd5bb2257de0
Charles Alaras <charles.alaras@gmail.com>
2022-08-08 14:35:12 -0700

    Add global authentication

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import GoogleIcon from '@mui/icons-material/Google';
 import NavBar from '../components/NavBar';
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 
 export default function Home() {
     const { data: session } = useSession();
@@ -19,19 +19,12 @@ export default function Home() {
         {session ? 
             <Card>
                 <CardContent>
-                    <Typography>You are logged in!</Typography>
+                    <Typography>Signed in as {session.user.email}</Typography>
                 </CardContent>
                 <Button onClick={() => signOut()}>Sign Out</Button>
             </Card>
-            :
-            <Card>
-                <CardContent>
-                    <Button variant="outlined" startIcon={<GoogleIcon/>} onClick={() => signIn()}>Sign in with Google</Button>
-                    <Typography>* Please use Google account provided by educational institution</Typography>
-                </CardContent>
-            </Card>
+            : <div>ERROR: Not logged in! How are you here?</div>
         }
         </div>
     )
 }
-
