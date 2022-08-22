c1e63c3ad7774c5ab767e3cc0cf5a18a69997c7e
Charles Alaras <charles.alaras@gmail.com>
2022-08-22 16:01:48 -0700

    Allow dynamic questions in API

--- pages/api/questions/[questionId].js
+++ pages/api/questions/[questionId].js
@@ -8,6 +8,17 @@ export default async(req, res) => {
     const question = await db
         .collection("questions")
         .findOne({ "_id": ObjectId(`${questionId}`)});
-    
+   
+    if(question.smart) { // If its dynamic, we need to fetch its params + results
+        const [params] = await db
+            .collection("variants")
+            .aggregate([
+                { $match: { parent: ObjectId(`${questionId}`)}},
+                { $sample: { size: 1 } }
+            ]).toArray();
+        const dynamicQuestion = { ...question, ...params };
+        res.json(dynamicQuestion);
+        return;
+    } // Otherwise, just give back the question data
     res.json(question);
 }
