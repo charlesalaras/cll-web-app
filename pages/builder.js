b4842473750a210adc3d35c273f113cf5b80365d
matchasaur <jtanu45@gmail.com>
2022-10-21 14:40:23 -0700

    Installed Mui Core packages and added builder functionality

--- pages/builder.js
+++ pages/builder.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
@@ -6,26 +7,89 @@ import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
+import { makeStyles } from '@material-ui/core'
 import Button from "@mui/material/Button";
+import Container from '@material-ui/core/Container'
 
 import Builder from "../components/BuilderForm";
 
+const useStyles = makeStyles({
+    field: {
+        marginTop: 20,
+        marginBottom: 20,
+        display: 'block'
+    },
+
+    buildContainer: {
+
+    }
+})
+
 export default function QuestionBuilder() {
+    const classes = useStyles()
+
+    const [questionTitle, setTitle] = useState('')
+    const [questionBody, setBody] = useState('')
+    const [questionType, setType] = useState('')
+
+    
     return(
-        <>
-        <Grid container spacing={2} alignItems="center">
-            <Grid item xs={5}>
-            <Paper variant="outlined">
-                <Typography variant="h2">Question Builder</Typography>
-                <Builder/>
-            </Paper>
+        <Grid container spacing={2} columnSpacing={2}>
+
+            <Grid item lg={6}>
+                <Paper>
+                  
+                <Typography
+                    variant="h6"
+                    component="h2"
+                    gutterBottom
+                >
+                    Question Builder
+                </Typography>
+
+                <form noValidate autoComplete="off">
+                    
+                    <TextField
+                        onChange={(e) => setTitle(e.target.value)}
+                        className={classes.field}
+                        label="Question Title"
+                        variant="filled"
+                        fullWidth
+                        required
+                    />
+
+                    <InputLabel>Question Type</InputLabel>
+                    <Select
+                        onChange={(e) => setType(e.target.value)}
+                        value={questionType}
+                        label="Question Type"
+                        variant="filled"
+                        fullWidth
+                        required
+                    >
+                        <MenuItem value={'MultipleChoice'}>Multiple Choice</MenuItem>
+                        <MenuItem value={'TrueFalse'}>True or False</MenuItem>
+                        <MenuItem value={'MultipleAnswer'}>Multiple Answers</MenuItem>
+                        <MenuItem value={'FillInTheBlank'}>Fill in the Blank</MenuItem>
+                    </Select>
+
+
+                    <TextField
+                        onChange={(e) => setBody(e.target.value)}
+                        className={classes.field}
+                        label="Question Body"
+                        variant="filled"
+                        multiline
+                        rows={5}
+                        fullWidth
+                        required
+                    />
+
+                </form>
+                </Paper>
             </Grid>
-            <Grid item xs={7}>
-            <Paper variant="outlined">
-                <Typography variant="h2">Question Preview</Typography>
-            </Paper>
-            </Grid>
+
+
         </Grid>
-        </>
     );
 }
